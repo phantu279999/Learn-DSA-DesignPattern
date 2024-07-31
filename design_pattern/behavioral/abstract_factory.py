5ec87c849df367b3ef1df66daf84ba4d41e93750
phantu279999 <phantu279999@gmail.com>
2024-07-31 11:13:13 +0700

    update code

--- design_pattern/behavioral/abstract_factory.py
+++ design_pattern/behavioral/abstract_factory.py
@@ -4,150 +4,150 @@ from abc import ABC, abstractmethod
 
 class FurnitureFactory(ABC):
 	@abstractmethod
-	def createChair(self):
-		return Chair()
+	def createChair(self) -> Chair:
+		pass
 
 	@abstractmethod
-	def createSofa(self):
-		return Sofa()
+	def createSofa(self) -> Sofa:
+		pass
 
 	@abstractmethod
-	def createCoffeeTable(self):
-		return CoffeeTable()
+	def createCoffeeTable(self) -> CoffeeTable:
+		pass
 
 
 class ArtDecoFurnitureFactory(FurnitureFactory):
-	def createChair(self):
+	def createChair(self) -> Chair:
 		return ArtDecoChair()
 
-	def createSofa(self):
+	def createSofa(self) -> Sofa:
 		return ArtDecoSofa()
 
-	def createCoffeeTable(self):
+	def createCoffeeTable(self) -> CoffeeTable:
 		return ArtDecoCoffeeTable()
 
 
 class VictorianFurnitureFactory(FurnitureFactory):
-	def createChair(self):
+	def createChair(self) -> Chair:
 		return VictorianChair()
 
-	def createSofa(self):
+	def createSofa(self) -> Sofa:
 		return VictorianSofa()
 
-	def createCoffeeTable(self):
+	def createCoffeeTable(self) -> CoffeeTable:
 		return VictorianCoffeeTable()
 
 
 class ModernFurnitureFactory(FurnitureFactory):
-	def createChair(self):
+	def createChair(self) -> Chair:
 		return ModernChair()
 
-	def createSofa(self):
+	def createSofa(self) -> Sofa:
 		return ModernSofa()
 
-	def createCoffeeTable(self):
+	def createCoffeeTable(self) -> CoffeeTable:
 		return ModernCoffeeTable()
 
 
 class Chair(ABC):
 	@abstractmethod
-	def hasLegs(self):
-		return "Chair 4 legs"
+	def hasLegs(self) -> str:
+		pass
 
 	@abstractmethod
-	def sitOn(self):
-		return "Chair sit on living room"
+	def sitOn(self) -> str:
+		pass
 
 
 class ArtDecoChair(Chair):
-	def hasLegs(self):
+	def hasLegs(self) -> str:
 		return "ArtDecoChair 4 legs"
 
-	def sitOn(self):
+	def sitOn(self) -> str:
 		return "ArtDecoChair sit on living room"
 
 
 class VictorianChair(Chair):
-	def hasLegs(self):
+	def hasLegs(self) -> str:
 		return "VictorianChair 4 legs"
 
-	def sitOn(self):
+	def sitOn(self) -> str:
 		return "VictorianChair sit on living room"
 
 
 class ModernChair(Chair):
-	def hasLegs(self):
+	def hasLegs(self) -> str:
 		return "ModernChair 4 legs"
 
-	def sitOn(self):
+	def sitOn(self) -> str:
 		return "ModernChair sit on living room"
 
 
 class Sofa(ABC):
 	@abstractmethod
-	def hasLegs(self):
-		return "Sofa 4 legs"
+	def hasLegs(self) -> str:
+		pass
 
 	@abstractmethod
-	def sitOn(self):
-		return "Sofa sit on living room"
+	def sitOn(self) -> str:
+		pass
 
 
-class ArtDecoSofa(Chair):
-	def hasLegs(self):
+class ArtDecoSofa(Sofa):
+	def hasLegs(self) -> str:
 		return "ArtDecoSofa 4 legs"
 
-	def sitOn(self):
+	def sitOn(self) -> str:
 		return "ArtDecoSofa sit on living room"
 
 
-class VictorianSofa(Chair):
-	def hasLegs(self):
+class VictorianSofa(Sofa):
+	def hasLegs(self) -> str:
 		return "VictorianSofa 6 legs"
 
-	def sitOn(self):
+	def sitOn(self) -> str:
 		return "VictorianSofa sit on living room"
 
 
-class ModernSofa(Chair):
-	def hasLegs(self):
+class ModernSofa(Sofa):
+	def hasLegs(self) -> str:
 		return "ModernSofa 6 legs"
 
-	def sitOn(self):
+	def sitOn(self) -> str:
 		return "ModernSofa sit on bedroom"
 
 
 class CoffeeTable(ABC):
 	@abstractmethod
-	def hasLegs(self):
-		return "CoffeeTable 4 legs"
+	def hasLegs(self) -> str:
+		pass
 
 	@abstractmethod
-	def sitOn(self):
-		return "CoffeeTable sit on living room"
+	def sitOn(self) -> str:
+		pass
 
 
-class ArtDecoCoffeeTable(Chair):
-	def hasLegs(self):
+class ArtDecoCoffeeTable(CoffeeTable):
+	def hasLegs(self) -> str:
 		return "ArtDecoCoffeeTable 4 legs"
 
-	def sitOn(self):
+	def sitOn(self) -> str:
 		return "ArtDecoCoffeeTable sit on living room"
 
 
-class VictorianCoffeeTable(Chair):
-	def hasLegs(self):
+class VictorianCoffeeTable(CoffeeTable):
+	def hasLegs(self) -> str:
 		return "VictorianCoffeeTable 6 legs"
 
-	def sitOn(self):
+	def sitOn(self) -> str:
 		return "VictorianCoffeeTable sit on living room"
 
 
-class ModernCoffeeTable(Chair):
-	def hasLegs(self):
+class ModernCoffeeTable(CoffeeTable):
+	def hasLegs(self) -> str:
 		return "ModernCoffeeTable 5 legs"
 
-	def sitOn(self):
+	def sitOn(self) -> str:
 		return "ModernCoffeeTable sit on living room"
 
 
@@ -161,7 +161,7 @@ class Client:
 		elif config['type'] == 'Modern':
 			self.factory = ModernFurnitureFactory()
 		else:
-			raise Exception("You provide type current hasn't")
+			raise Exception("You provided a type that is not supported")
 
 	def main(self) -> None:
 		product_a = self.factory.createChair()
@@ -181,4 +181,3 @@ if __name__ == '__main__':
 	}
 	client = Client(config)
 	client.main()
-
